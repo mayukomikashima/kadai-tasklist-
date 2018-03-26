38f7ecd724d0c87edcb663200a2c5c647b40357a
mayuko <mksmmayuko@gmail.com>
2018-03-26 13:51:27 +0000

    app/controllers/tasks_controller.rb

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -33,7 +33,7 @@ class TasksController < ApplicationController
   end
 
   def update
-      if @task.update(message_params)
+      if @task.update(task_params)
       flash[:success] = 'Task は正常に更新されました'
       redirect_to @task
     else
